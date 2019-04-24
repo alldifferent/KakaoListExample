154e1c0e6e8c3d6f2c4bb25a74b08a8ab33c1c03
alldifferent <alldifferent@naver.com>
2019-04-24 18:01:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kakaolistexample/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.kakaolistexample;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kakaolistexample.databinding.ActivityMainBinding;
+import com.example.kakaolistexample.datas.KakaoChat;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding act;
+    List<KakaoChat> kakaoList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        setupEvents();
+        setValues();
+        fillKakao();
+
+
+    }
+
+    void setupEvents(){
+
+
+    }
+
+    void setValues(){
+
+    }
+
+    public static String timeHMS(){
+
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+        return sdf.format(cal.getTime());
+
+    }
+
+    void fillKakao(){
+
+        kakaoList.add(new KakaoChat("alldi", "안녕하세요.", timeHMS()));
+        kakaoList.add(new KakaoChat("eiww", "Hello.", timeHMS()));
+        kakaoList.add(new KakaoChat("fffww", "yo.", timeHMS()));
+        kakaoList.add(new KakaoChat("zqqqw", "what are you doing.", timeHMS()));
+        kakaoList.add(new KakaoChat("rrddqqqq", "안녕하세요.", timeHMS()));
+        kakaoList.add(new KakaoChat("gggddd", "kkkk.", timeHMS()));
+
+    }
+
+}
---
+++ app/src/main/java/com/example/kakaolistexample/datas/KakaoChat.java
@@ -0,0 +1,14 @@
+package com.example.kakaolistexample.datas;
+
+public class KakaoChat {
+
+    public String userID;
+    public String message;
+    public String date;
+
+    public KakaoChat(String userID, String message, String date) {
+        this.userID = userID;
+        this.message = message;
+        this.date = date;
+    }
+}
